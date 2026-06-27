470bb9d7ef2c85d074434eb31f3ab69669910ceb
Krishu <krishukumar72@gmail.com>
2026-06-28 01:53:14 +0530

    4th commit

---
+++ 12-Two_Way-Binding/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [title , setTitle] = useState('')
+
+  const SubmitHandler = (data) =>{
+    data.preventDefault()
+    console.log('form submitted by', title);
+
+    setTitle('')
+  }
+  return (
+    <div>
+      <form onSubmit={(data) =>{
+        SubmitHandler(data)
+      }}>
+        <input 
+        type="text" 
+        placeholder='Enter your name'
+        value={title}
+        onChange={(data)=>{
+          setTitle(data.target.value);
+        }}
+        />
+        <button>Submit</button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default App
